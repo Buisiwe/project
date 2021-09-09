3a863487c237efbeb0a0c2d3116d84bae216c2ea
Ruqayat <yinkaajibade97@gmail.com>
2021-09-09 05:34:19 +0100

    update

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,12 @@ import "@fontsource/mulish";
 import './styles/index.css';
 import React, { useState } from 'react';
 import Register from './pages/register';
-import Header from './components/header';
+// import Header from './components/header';
 import Login from './pages/login';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Password from './pages/password';
+// import Password from './pages/password';
 import ResetPassword from "./pages/resetpassword";
+import Onboarding from "./pages/onboarding";
 import LandingPage from './pages/landingpage';
 import Prediction from './pages/prediction';
 
@@ -28,6 +29,9 @@ function App() {
 						</Route>
 						<Route exact path='/'>
 							<LandingPage />
+						</Route>
+						<Route exact path='/onboarding'>
+							<Onboarding />
 						</Route>
 						<Route exact path='/prediction'>
 							<Prediction />
