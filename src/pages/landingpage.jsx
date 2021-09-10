e797c16902e8952df5afdd42220b8ad4a8f302a5
Taiwo <famaktaiwo7@gmail.com>
2021-09-10 02:41:08 +0100

    first commit

--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AdvantagesBar from '../components/advantages-bar';
-// import ReactLogo from './logo.svg';
 import TopNavigation from '../components/top-navigation';
 import '../styles/index.css';
 import '../styles/landingpage.css';
@@ -8,11 +7,11 @@ import '../styles/landingpage.css';
 function LandingPage() {
 	return (
 		<>
-		<img src="" alt='logo' />
 			<div>
 				<TopNavigation />
 			</div>
 
+<div className='welcome'>
 			<div className='intro'>
 				<div className='headline'>
 					<h1 className='heading'>
@@ -35,6 +34,7 @@ function LandingPage() {
 					src='https://s3-alpha-sig.figma.com/img/5a71/8938/cbd96e01169ecaccfe108988c59eb3ce?Expires=1631491200&Signature=SoBLBgGqeLkKn~0AEFaXcoNeJbHONU6vqDFOixIfgXtlANlehEohQIshm2FH0osiOL9nsGlIhQ0hklQBb5IvYWl25wkDTFEtOkLSsh9HX2K-xoXwIDEsKy3hU61x7PKKLGr7V2xM12AWkcOZfHJdhI2mbjlPVPxVo6ymUE3n~R2MY8RnZJ3XNjMRkc5wYF~XxhVI0n68gOp~NqTHCayQBGbU9PUHrMateqv5pui0ukD93xdSV6iZUNptoQYYW6lHXCh-dJxnYYxanzTEjskIgC7XbdafXxRfx5YjJYwH8SD5sl8T5aX~4Yz8S9yXUmqmR6D4u9MrLGGF2r9FM3Iypw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
 					alt=''
 				/>
+			</div>
 			</div>
 
 			<div className='about-section'>
