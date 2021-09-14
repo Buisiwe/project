25a4cb89c3a9f6836970806fa527faab90f4844b
Taiwo <famaktaiwo7@gmail.com>
2021-09-14 21:18:06 +0100

    update

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import "@fontsource/mulish";
 import "./styles/index.css";
 import React, { useState } from "react";
 import Register from "./pages/register";
-import Header from './components/header';
+// import Header from './components/header';
 import Login from "./pages/login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Password from './pages/password';
@@ -20,7 +20,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+        {/* <Header /> */}
         <div className="container d-flex align-items-center flex-column">
           <Switch>
             <Route path="/register" exact={true}>
--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/contactpage.css';
 import Logo from '../components/logo';
 
 
@@ -9,8 +10,10 @@ function ContactPage (){
 <div>
     <Logo />
 </div>
-<div className='header-image'>
-    //header image
+<div className='header'>
+<img src='https://live.staticflickr.com/65535/51473112767_509b4b0ca7_b.jpg' alt='header-image' className='header-image'/>
+
+
 </div>
 <h2>Get in touch by filling out this form</h2>
 </>
