b52848493c793fa78f05572d0db0b8f6d7beffa2
Taiwo <famaktaiwo7@gmail.com>
2021-09-17 14:22:29 +0100

    commit

--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -56,7 +56,7 @@ const ContactPage = () => {
 </div>
        
       </div>
-      <button type="submit">{status}</button>
+      <button className='submit-button' type="submit">{status}</button>
     </form>
 
 <img src='https://s3-alpha-sig.figma.com/img/5a71/8938/cbd96e01169ecaccfe108988c59eb3ce?Expires=1631491200&Signature=SoBLBgGqeLkKn~0AEFaXcoNeJbHONU6vqDFOixIfgXtlANlehEohQIshm2FH0osiOL9nsGlIhQ0hklQBb5IvYWl25wkDTFEtOkLSsh9HX2K-xoXwIDEsKy3hU61x7PKKLGr7V2xM12AWkcOZfHJdhI2mbjlPVPxVo6ymUE3n~R2MY8RnZJ3XNjMRkc5wYF~XxhVI0n68gOp~NqTHCayQBGbU9PUHrMateqv5pui0ukD93xdSV6iZUNptoQYYW6lHXCh-dJxnYYxanzTEjskIgC7XbdafXxRfx5YjJYwH8SD5sl8T5aX~4Yz8S9yXUmqmR6D4u9MrLGGF2r9FM3Iypw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA' alt=''
