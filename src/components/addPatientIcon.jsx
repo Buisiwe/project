d1c3831304f2756dd65efc15dc97d723b5d1c1ad
Ruqayat <yinkaajibade97@gmail.com>
2021-09-21 11:30:06 +0100

    add-patient-btn

--- src/components/addPatientIcon.jsx
+++ src/components/addPatientIcon.jsx
@@ -1,12 +1,18 @@
 import "../styles/dashboard.css";
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import { useHistory } from "react-router";
 
 export default function AddPatientIcon() {
-
+    const history = useHistory();
+        function addNewPatient(e){
+            e.preventDefault();
+            console.log('You clicked submit.');
+            history.push('/patient')
+        }
     return(
-    <div id="add-patient-icon">
-        <PersonAddIcon/>
+    <button type="submit" id="add-patient-icon" onClick={addNewPatient}>
+        <PersonAddIcon fontSize='large'/>
         <span>Add Patient</span>
-    </div>
+    </button>
     )
 }
