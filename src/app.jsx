e62bd8eade3fe762bd4721209dad0db193a1d6f8
Taiwo <famaktaiwo7@gmail.com>
2021-10-03 14:04:26 +0100

    Co-authored-by: sanicalapp <sanicalapp@users.noreply.github.com>

--- src/app.jsx
+++ src/app.jsx
@@ -1,50 +1,79 @@
-import "@fontsource/mulish";
-import "./styles/index.css";
-import React, {useEffect} from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import StateProvider from "./components/stateprovider";
-import Onboarding from "./pages/onboarding";
-import Register from "./pages/register";
-import Login from "./pages/login";
-import ResetPassword from "./pages/resetpassword";
-import PasswordChange from "./pages/passwordchange";
+import '@fontsource/mulish';
+import './styles/index.css';
+import React, { useEffect, useState } from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
+import StateProvider from './components/stateprovider';
+import Onboarding from './pages/onboarding';
+import Register from './pages/register';
+import Login from './pages/login';
+import ResetPassword from './pages/resetpassword';
+import PasswordChange from './pages/passwordchange';
 import LandingPage from './pages/landingpage';
 import ContactPage from './pages/contactpage';
 import TeamPage from './pages/teampage';
 import Prediction from './pages/prediction';
 // import Topbar from "./pages/topbar";
-import Dashboard from "./pages/dashboard";
-import Patient from "./pages/patientProfile";
-import PatientDataDisplay from "./pages/patientDataDisplay";
+import Dashboard from './pages/dashboard';
+import Patient from './pages/patientProfile';
+import PatientDataDisplay from './pages/patientDataDisplay';
 // import PatientDataHistory from "./pages/PatientDataHistory";
-import Result from "./pages/result";
-
+import Result from './pages/result';
 
 function App() {
+	const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
 
-	const history = useHistory();
-	useEffect(()=>{
-		const accessToken = localStorage.getItem("accessToken")
-		console.log("This is the ", accessToken)
-		if(!accessToken){
-			console.log("This worked")
-			history.push("/login")
+	useEffect(() => {
+		const accessToken = localStorage.getItem('accessToken');
+
+		if(accessToken){
+			fetch('http://envisio-001-site1.itempurl.com/api/v1/User/get-user',  {
+                headers: {
+                    'content-type': 'application/json',
+					'Authorization': `Bearer ${accessToken}`
+                }
+            })
+			.then(response => response.json())
+			.then(userData => {
+				console.log("this is the data ==> ", userData)
+				setIsUserAuthenticated(true)
+				// TODO: store user data in your data store 
+
+			})
+			.catch(err => {
+				// TODO: show user error with a popup or something
+				setIsUserAuthenticated(false);
+				localStorage.removeItem('accessToken');
+			})
+
 		}
-	})
+	}, []);
+
 	return (
 		<StateProvider>
 			<Router>
-				<div className="App">
-					<div className="container d-flex align-items-center flex-column">
+				<div className='App'>
+					<div className='container d-flex align-items-center flex-column'>
 						<Switch>
-							<Route path="/register" exact={true} component={Register} />
+							<Route path='/register' exact={true} component={Register} />
 
-							<Route path="/login" exact={true} component={Login} />
+							<Route path='/login' exact={true} component={Login} />
 
-							<Route path="/reset_password" exact={true} component={ResetPassword} />
+							<Route
+								path='/reset_password'
+								exact={true}
+								component={ResetPassword}
+							/>
 
-							<Route path="/password_change" exact={true} component={PasswordChange} />
+							<Route
+								path='/password_change'
+								exact={true}
+								component={PasswordChange}
+							/>
 
 							<Route exact path='/' component={LandingPage} />
 
@@ -59,19 +88,24 @@ function App() {
 
 							{/* <Route exact path='/topbar' component={Topbar} /> */}
 
-							<Route exact path='/dashboard' component={Dashboard} />
+							<Route exact path='/dashboard' >
+								{isUserAuthenticated ? <Dashboard /> : <Redirect to='/login' />}
+							</Route>
 
 							<Route exact path='/patient' component={Patient} />
-							
-							{/* <Route exact path='/patient-history' component={PatientDataHistory} /> */}
-							<Route exact path='/patient-data' component={PatientDataDisplay} />
 
+							{/* <Route exact path='/patient-history' component={PatientDataHistory} /> */}
+							<Route
+								exact
+								path='/patient-data'
+								component={PatientDataDisplay}
+							/>
 						</Switch>
 					</div>
 				</div>
 			</Router>
 		</StateProvider>
-	)
+	);
 }
 
 export default App;
