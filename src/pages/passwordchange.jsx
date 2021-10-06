31f3565d27f8ea0b1d94502b1c2acdb7a1574d0b
Buisiwe <buisiwemad@gmail.com>
2021-10-06 10:06:42 +0200

    Merge pull request #37 from Women-Techsters-Fellowship-2021/dashboard
    styling adjustments

--- src/pages/passwordchange.jsx
+++ src/pages/passwordchange.jsx
@@ -1,18 +1,95 @@
 import { useForm } from "react-hook-form";
+import { useHistory } from "react-router";
 import '../styles/register.css';
+import swal from 'sweetalert';
 import Header from '../components/header';
 import SideColor from '../components/sidecolor';
 import useMatchMedia from "../custom hooks/matchmedia";
 
 
+import React, { useState } from "react"; 
+import validator from 'validator'
+
+
 function PasswordChange() {
+    const history = useHistory();
     const { register, handleSubmit } = useForm();
-    const registerUser = ({ email, password, confirmPassword }) => {
-        //  confirm if passowrds entered match
-        if (password !== confirmPassword) {
-            return alert("The password entered does not match");
-        }
+   const [errorMessage, setErrorMessage] = useState('') 
+
+  
+
+  const validate = (value) => { 
+
+  
+
+    if (validator.isStrongPassword(value, { 
+
+      minLength: 8, minLowercase: 1, 
+
+      minUppercase: 1, minNumbers: 1, minSymbols: 1 
+
+    })) { 
+
+      setErrorMessage('Is Strong Password') 
+      
+
+    } else { 
+
+      setErrorMessage('Is Not Strong Password') 
+
+    } 
+
+  } 
+
+const newpasswordHandler = async ({email, newPassword, ConfirmnewPassword}) => {
+
+    // let isValid = true;
+            
+ 
+
+
+          let userEmail = {
+              email: email,
+              newPassword: newPassword,
+              ConfirmnewPassword: ConfirmnewPassword,
+           
+        };
+
+
+    // perform all neccassary validations
+    if (newPassword !== ConfirmnewPassword) {
+        alert("Passwords don't match");
+    } 
+    else {
+        // make API call
+        fetch(
+            'http://envisio-001-site1.itempurl.com/api/v1/Auth/ResetPassword',
+            {
+                method: 'Post',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(userEmail ),
+            }
+        )
+            .then(res => res.text())
+            .then(result => {
+                console.log(result)
+            });
+           history.push('/login'); 
+            swal({
+                title: 'Password Created Successful',
+                text: " ",
+                icon: "success",
+                button: "Close" ,
+                
+            })
+            ;
     }
+
+        
+    };
+
     const isDesktopResolution = useMatchMedia('(min-width:768px)', true)
     return (
         <>
@@ -26,34 +103,62 @@ function PasswordChange() {
                         <h2 className="main_title ">Reset Password</h2>
                         <p className="center">You're almost there</p>
                         <br />
-                        <form onSubmit={handleSubmit(registerUser)}>
-                            <div className="input-container">
+                        <form onSubmit={handleSubmit(newpasswordHandler)}>
+
+                         <div className="input-container">
                                 <input
-                                    id="password"
+                                    id="email"
                                     className="input"
-                                    type="password"
-                                    {...register("password", { required: true })}
+                                    type="text"
+                                    {...register("email", { required: true })}
                                     placeholder=" "
                                 />
                                 <div className="cut" />
-                                <label htmlFor="Password" className="placeholder">
-                                    Password
+                                <label htmlFor="email" className="placeholder">
+                                    Re-Enter Email Address
                                 </label>
                             </div>
+
                             <div className="input-container">
                                 <input
-                                    id="confirmpassword"
+                                    id="newPassword"
                                     className="input"
                                     type="password"
-                                    {...register("confirmpassword", { required: true })}
+                                    {...register("newPassword", { required: true })}
+                                    onChange={(e)=> validate(e.target.value)}
                                     placeholder=" "
                                 />
+                                <br/>
+                                 <span style={{fontWeight:'bold',
+                            color: 'red',
+                            }}>{errorMessage}</span>
+
                                 <div className="cut" />
-                                <label htmlFor="Password" className="placeholder">
+                                <label htmlFor="password" className="placeholder">
+                                    Password
+                                </label>
+                               
+                            </div>
+                            <br/>
+                            <div className="input-container">
+                                  <input
+                                    id="ConfirmnewPassword"
+                                    className="input"
+                                    type="password"
+                                    {...register("ConfirmnewPassword", { required: true })}
+                                    onChange={(e)=> validate(e.target.value)}
+                                    placeholder=" "
+                                />
+                                <br/>
+                                <span style={{fontWeight:'bold',
+                            color: 'red',
+                            }}>{errorMessage}</span>
+                                <div className="cut" />
+                               <label htmlFor="Password" className="placeholder" >
                                     Confirm Password
                                 </label>
                             </div>
-                            <button type="submit" className="form-submit">
+                            <button type="submit" className="form-submit-create-new-password">
                                 Confirm
                             </button>
                         </form>
